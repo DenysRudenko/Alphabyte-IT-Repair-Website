6a84d3eed67b5053ec718a12945f9d3e215ae347
DenysRudenko <heartbreathing19951014@gmail.com>
2024-04-19 19:55:36 +0000

    Update(php): updated the pagination functionality on the shop page

--- assets/html/shop.php
+++ assets/html/shop.php
@@ -23,17 +23,51 @@ if(isset($_POST['search'])){
 
   // return all products
 }else {
-  // Database Querry 
-$stmt = $conn->prepare("SELECT * FROM products");
+  
+  // Pagination
+  if(isset($_GET['page_no']) && $_GET['page_no'] != "") {
 
-// Run the querry
-$stmt->execute();
+    // if user has already entered page then page number is the one that selected
+    $page_no = $_GET['page_no'];
+  } else {
 
-// Get results and add them to stmt variable
-$products = $stmt->get_result();
+    // if user just entered the page then default page is 1
+    $page_no = 1;
+  }
+
+  // return number of products 
+  $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products ");
+
+  $stmt1->execute();
+
+  $stmt1->bind_result($total_records);
+
+  $stmt1->store_result();
+
+  $stmt1->fetch();
+
+  // set the total number of products on the page
+
+  $total_records_per_page = 8;
+
+  $offset = ($page_no - 1) * $total_records_per_page;
+
+  $previous_page = $page_no - 1;
+  $next_page = $page_no + 1;
+
+  $adjacents = "2";
+
+  $total_number_of_pages = ceil($total_records / $total_records_per_page);
+
+  // get all products
+
+  $stmt2 = $conn->prepare("SELECT * FROM products LIMIT $offset, $total_records_per_page");
+
+  $stmt2->execute();
+
+  $products = $stmt2->get_result();
+
 }
-
-
 
 
 ?>
@@ -128,21 +162,31 @@ $products = $stmt->get_result();
 
       <?php } ?>
 
-
       <!-- Pagination Bar -->
       <nav aria-label="Page navigation example">
-        <ul class="pagination mt-5 ">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        <ul class="pagination mt-5 mx-auto">
+
+        <li class="page-item <?php if($page_no <= 1){ echo 'disabled'; } ?>">
+        <a class="page-link" href="<?php if($page_no > 1){ 
+          echo "?page_no=".($page_no - 1); } else { echo "#"; } ?>">Previous</a>
+        </li>
+
+            <li class="page-item"><a class="page-link" href="?page_no=1">1</a></li>
+            <li class="page-item"><a class="page-link" href="?page_no=2">2</a></li>
+
+            <?php if($page_no >= 3) { ?>
+            <li class="page-item disabled"><span class="page-link">...</span></li>
+            <li class="page-item active"><span class="page-link"><?php echo $page_no; ?></span></li>
+            <?php } ?>
+
+
+            <li class="page-item <?php if($page_no >= $total_number_of_pages){ echo 'disabled'; } ?>">
+            <a class="page-link" href="<?php if($page_no < $total_number_of_pages){ echo "?page_no=".($page_no + 1); } else { echo "#"; } ?>">Next</a>
+            </li>
         </ul>
       </nav>
     </div>
   </section>
-
-
 
 <?php 
 
