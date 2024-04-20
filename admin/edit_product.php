99cfddd7e12c20b3f84a3e3235b2d4e6edca1e29
DenysRudenko <heartbreathing19951014@gmail.com>
2024-04-20 13:33:40 +0000

    Update(php): created a functinality for edit product

--- admin/edit_product.php
+++ admin/edit_product.php
@@ -1,18 +1,47 @@
-<?php include('header.php'); ?>
+<?php include('header.php'); 
 
-<?php 
 
-if($_GET['product_id']) {
+if(isset($_GET['product_id'])) {
+
     $product_id = $_GET['product_id'];
     $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
     $stmt->bind_param('i', $product_id);
     $stmt->execute();
     $products = $stmt->get_result();
+
+} else if(isset($_POST['edit_product'])) {
+
+    $product_id = $_POST['product_id'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $offer = $_POST['offer'];
+    $color = $_POST['color'];
+    $category = $_POST['category'];
+
+    $stmt = $conn->prepare(
+        'UPDATE products SET product_name = ?, product_description = ?, product_price = ?,
+        product_special_offer = ?, product_color = ?, product_category = ? WHERE product_id = ? ');
+  
+    $stmt->bind_param("ssssssi", $title, $description, $price, $offer, $color, $category, $product_id);
+
+
+    if($stmt->execute()) {
+
+        header('location: products.php?edit_success_message=Product has been update successfully!');
+
+    } else {
+        
+        header('location: products.php?edit_failure_message=Error occured, try again!');
+
+    }
+
+    
+
 }else {
     header('products.php');
     exit;
 }
-
 
 ?>
 
@@ -34,12 +63,15 @@ if($_GET['product_id']) {
       <div class="table-responsive">
 
 
-      <div class="mx-auto container">
-    <form id="edit-form" enctype="multipart/form-data" method="post" action="edit_product.php">
+ <div class="mx-auto container">
+    <form id="edit-form" method="POST" action="edit_product.php">
     <p style="color: red;"><?php if(isset($_GET['error'])){ echo $_GET['error']; } ?></p>
     <div class="form-group mt-2">
 
         <?php foreach($products as $product) { ?>
+        
+      <input type="hidden" name="product_id" value="<?php echo $product['product_id']; ?>">
+
       <label for="product-title">Title</label>
       <input type="text" class="form-control" id="product-title" value="<?php echo $product['product_name']; ?>" name="title" placeholder="Title" required>
     </div>
@@ -51,7 +83,7 @@ if($_GET['product_id']) {
 
     <div class="form-group mt-2">
       <label for="product-price">Price</label>
-      <input type="number" class="form-control" id="product-price" value="<?php echo $product['product_price']; ?>" name="price" placeholder="Price">
+      <input type="text" class="form-control" id="product-price" value="<?php echo $product['product_price']; ?>" name="price" placeholder="Price" required>
     </div>
 
     <div class="form-group mt-2">
@@ -75,7 +107,7 @@ if($_GET['product_id']) {
     </div>
 
     <div class="form-group mt-2">
-      <input type="submit" class="btn btn-primary" name="edit_product" value="Edit">
+    <input type="submit" class="btn btn-primary" name="edit_product" value="Edit">
     </div>
 
     <?php } ?>
